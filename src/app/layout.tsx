7f1a5006702d817504e0c8b71f003ea20a3e3dfd
gavinvc <tzn4fx@virginia.edu>
2025-08-07 06:46:11 -0400

    hopefully final analytics fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Analytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
